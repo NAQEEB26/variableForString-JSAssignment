88f12ed61172920d1976f098b886041d1b98356e
NAQEEB UL REHMAN <130925302+NAQEEB26@users.noreply.github.com>
2023-12-03 14:03:27 +0500

    Add files via upload

---
+++ Assigment no 2/app.js
@@ -0,0 +1,39 @@
+// Question no 1
+var UserNumber
+
+// Question no 2
+var  myName = "Naqeeb ul Rehman";
+ 
+// Question no 3
+var message = " Hello World";
+alert(message);
+
+// Question no 4
+alert("Naqeeb ul rehman Attari");
+alert("I am 19 Years old");
+alert("Web Developer");
+
+
+// Question no 5
+alert("Pizza \n pizz \n piz \n pi \n p");
+
+// Question no 6
+var Email="www.example@gmail.com";
+alert("My Email is " + Email)
+
+// Question no 7
+var Book= "A smater way to learn JavaScript"
+alert("I am trying to leran from the Book " + Book);
+
+// Question no 8
+var content= "Hey! I can HTML content through Javascript"
+document.write(content);
+
+// Question no 9
+
+var style = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(style);
+
+
+// Assingmet Complete
+
